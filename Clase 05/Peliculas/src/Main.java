34e13e4e61cd8655783fa9583569adb87831e265
ErickVillarroel0022 <erickvillarroel0022@gmail.com>
2023-05-30 09:47:35 -0500

    no message

--- Clase 05/Peliculas/src/Main.java
+++ Clase 05/Peliculas/src/Main.java
@@ -10,3 +10,58 @@
  * Cada uno debe crear 3 instancias de la clase Peliculas y deben hacer los push
  * respectivos para que se note su aporte.
  */
+import java.util.Scanner;
+
+public class menuPrincipal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Peliculas rock = new Peliculas("rock",
+                "120bpm",
+                1990,
+                "Three grace days",
+                "Canada");
+
+        Peliculas metal = new Peliculas("Metal",
+                "120bpm",
+                1990,
+                "Marilyn Manson",
+                "USA");
+
+        Peliculas jazz = new Peliculas("Jazz",
+                "70bpm",
+                1950,
+                "Eric",
+                "USA");
+
+        Peliculas pop = new Peliculas("Pop",
+                "100bpm",
+                1980,
+                "Michael Jackson",
+                "USA");
+
+
+
+        System.out.println("Llena los datos de tu género favorito: ");
+        System.out.println("Tipo: ");
+        String tipo = scanner.nextLine();
+        System.out.println("Ritmo: ");
+        String ritmo = scanner.nextLine();
+        System.out.println("Año: ");
+        int anio = scanner.nextInt();
+        scanner.nextLine(); // Limpiar el búfer
+        System.out.println("País: ");
+        String pais = scanner.nextLine();
+        System.out.println("Artista: ");
+        String artista = scanner.nextLine();
+
+        Peliculas favorito = new Peliculas(tipo, ritmo, anio, artista, pais);
+
+        System.out.println("Datos de tu género favorito:");
+        System.out.println(favorito.tipo);
+        System.out.println(favorito.getAnio());
+        System.out.println(favorito.getPais());
+        System.out.println(favorito.getArtista());
+    }
+}
+
