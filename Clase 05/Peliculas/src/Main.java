b2273484994c6c3aaf08a423f7e24322592a8c43
Mateo <minora.mateo@gmail.com>
2023-05-30 09:29:30 -0500

    no message

---
+++ Clase 05/Peliculas/src/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Dada la temática: Películas, implementar un proyecto con al menos 12
+ * peliculas.
+ * 
+ * El proyecto debe ser implementado por máximo 3 personas.
+ * 
+ * Uno de los integrantes debe crear el proyecto y los demás deben clonar el
+ * proyecto.
+ * 
+ * Cada uno debe crear 3 instancias de la clase Peliculas y deben hacer los push
+ * respectivos para que se note su aporte.
+ */
