deac61afcc7b73db336f2f30e95ea6093ee0715a
Mateo_Mino <minora.mateo@gmail.com>
2023-06-01 23:24:55 -0500

    no message

--- Clase 05/Peliculas/src/MenuPrincipal.java
+++ Clase 05/Peliculas/src/MenuPrincipal.java
@@ -10,58 +10,221 @@
  * Cada uno debe crear 3 instancias de la clase Peliculas y deben hacer los push
  * respectivos para que se note su aporte.
  */
+
+//Librerias
 import java.util.Scanner;
 
+//clase publica menu principal//
 public class MenuPrincipal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Peliculas rock = new Peliculas("rock",
-                "120bpm",
-                1990,
-                "Three grace days",
-                "Canada");
+        //Peliculas
+        Peliculas Interestellar = new Peliculas(
+                "Interestellar",
+                "Accion ",
+                2014,
+                "Christopher Nolan ",
+                "Estados Unidos");
 
-        Peliculas metal = new Peliculas("Metal",
-                "120bpm",
-                1990,
-                "Marilyn Manson",
-                "USA");
+        Peliculas Contratiempo = new Peliculas(
+                "Contratiempo",
+                "Suspenso",
+                2016,
+                "Oriol Paulo",
+                "España");
 
-        Peliculas jazz = new Peliculas("Jazz",
-                "70bpm",
-                1950,
-                "Eric",
-                "USA");
+        Peliculas Batman = new Peliculas(
+                "Batman",
+                "Accion",
+                2005,
+                "Christopher Nolan ",
+                "Estados Unidos");
 
-        Peliculas pop = new Peliculas("Pop",
-                "100bpm",
-                1980,
-                "Michael Jackson",
-                "USA");
+        Peliculas Spiderman = new Peliculas(
+                "Spiderman",
+                "Accion",
+                2002,
+                "Sam Raimi",
+                "Estados Unidos");
+
+        /*###############################################################################################*/
+
+        Peliculas ElTiempoContigo = new Peliculas(
+                "El tiempo contigo",
+                "Animacion",
+                2019,
+                "Makoto Shinkai",
+                "Japon");
+
+        Peliculas YourName = new Peliculas(
+                "Your Name",
+                "Romance",
+                2016,
+                "Makoto Shinkai",
+                "Japon");
+
+        Peliculas ElCodigoDaVinici = new Peliculas(
+                "El Codigo Da Vinci",
+                "Suspenso",
+                2006,
+                "Ron Howard",
+                "Francia");
+
+        Peliculas DragonBallSuperBroly = new Peliculas(
+                "Dragon Ball Super: Broly",
+                "Animacion",
+                2018,
+                "Tatsuya Nagamine",
+                "Japon");
+
+        /*###############################################################################################*/
+
+        Peliculas ElExorcista = new Peliculas(
+                "El Exorcista",
+                "Terror",
+                1973,
+                "William Peter Blatty",
+                "Estados Unidos");
+
+        Peliculas Terrifier2 = new Peliculas(
+                "Terrifier 2",
+                "Horror",
+                2022,
+                "Damien Leone",
+                "Estados Unidos");
+
+        Peliculas Elpayasodelmal = new Peliculas(
+                "El payaso del mal",
+                "Horror",
+                2014,
+                "Jon Watts",
+                "Estados Unidos");
+
+        Peliculas It = new Peliculas(
+                "It (Eso)",
+                "Horror",
+                2017,
+                "Andrés Muschietti",
+                "Estados Unidos");
+
+        /*###############################################################################################*/
+
+        /*###############################################################################################*/
+
+        System.out.println("\n BASE DE DATOS DE PELICULAS");
+        System.out.println("\n Seleccion de Peliculas\n");
+        System.out.println("\n 1) Interestellar");
+        System.out.println("\n 2) Contratiempo");
+        System.out.println("\n 3) Batman");
+        System.out.println("\n 4) Spiderman");
+        System.out.println("\n 5) El tiempo contigo");
+        System.out.println("\n 6) Your Name");
+        System.out.println("\n 7) El Codigo Da Vinci");
+        System.out.println("\n 8) Dragon Ball Super: Broly");
+        System.out.println("\n 9) El Exorcista");
+        System.out.println("\n 10) Terrifier 2");
+        System.out.println("\n 11) El payaso del mal");
+        System.out.println("\n 12) It (Eso)");
 
 
+        Scanner Entrada = new Scanner(System.in);
 
-        System.out.println("Llena los datos de tu género favorito: ");
-        System.out.println("Tipo: ");
-        String tipo = scanner.nextLine();
-        System.out.println("Ritmo: ");
-        String ritmo = scanner.nextLine();
-        System.out.println("Año: ");
-        int anio = scanner.nextInt();
-        scanner.nextLine(); // Limpiar el búfer
-        System.out.println("País: ");
-        String pais = scanner.nextLine();
-        System.out.println("Artista: ");
-        String artista = scanner.nextLine();
+        System.out.print("Ingrese un número del 1 al 12: ");
+        int numero = scanner.nextInt();
+        switch (numero) {
+            case 1:
+                System.out.println("Opción 1 seleccionada.");
+                System.out.println(Interestellar.getNombre());
+                System.out.println(Interestellar.getAnio());
+                System.out.println(Interestellar.getDirector());
+                System.out.println(Interestellar.getPais());
+                break;
+            case 2:
+                System.out.println("Opción 2 seleccionada.");
+                System.out.println(Contratiempo.getNombre());
+                System.out.println(Contratiempo.getAnio());
+                System.out.println(Contratiempo.getDirector());
+                System.out.println(Contratiempo.getPais());
+                break;
+            case 3:
+                System.out.println("Opción 3 seleccionada.");
+                System.out.println(Batman.getNombre());
+                System.out.println(Batman.getAnio());
+                System.out.println(Batman.getDirector());
+                System.out.println(Batman.getPais());
 
-        Peliculas favorito = new Peliculas(tipo, ritmo, anio, artista, pais);
+                break;
+            case 4:
+                System.out.println("Opción 4 seleccionada.");
+                System.out.println(Spiderman.getNombre());
+                System.out.println(Spiderman.getAnio());
+                System.out.println(Spiderman.getDirector());
+                System.out.println(Spiderman.getPais());
+                break;
+            case 5:
+                System.out.println("Opción 5 seleccionada.");
+                System.out.println(ElTiempoContigo.getNombre());
+                System.out.println(ElTiempoContigo.getAnio());
+                System.out.println(ElTiempoContigo.getDirector());
+                System.out.println(ElTiempoContigo.getPais());
+                break;
+            case 6:
+                System.out.println("Opción 6 seleccionada.");
+                System.out.println(YourName.getNombre());
+                System.out.println(YourName.getAnio());
+                System.out.println(YourName.getDirector());
+                System.out.println(YourName.getPais());
+                break;
+            case 7:
+                System.out.println("Opción 7 seleccionada.");
+                System.out.println(ElCodigoDaVinici.getNombre());
+                System.out.println(ElCodigoDaVinici.getAnio());
+                System.out.println(ElCodigoDaVinici.getDirector());
+                System.out.println(ElCodigoDaVinici.getPais());
+                break;
+            case 8:
+                System.out.println("Opción 8 seleccionada.");
+                System.out.println(DragonBallSuperBroly.getNombre());
+                System.out.println(DragonBallSuperBroly.getAnio());
+                System.out.println(DragonBallSuperBroly.getDirector());
+                System.out.println(DragonBallSuperBroly.getPais());
+                break;
+            case 9:
+                System.out.println("Opción 9 seleccionada.");
+                System.out.println(ElExorcista.getNombre());
+                System.out.println(ElExorcista.getAnio());
+                System.out.println(ElExorcista.getDirector());
+                System.out.println(ElExorcista.getPais());
+                break;
+            case 10:
+                System.out.println("Opción 10 seleccionada.");
+                System.out.println(Terrifier2.getNombre());
+                System.out.println(Terrifier2.getAnio());
+                System.out.println(Terrifier2.getDirector());
+                System.out.println(Terrifier2.getPais());
+                break;
+            case 11:
+                System.out.println("Opción 11 seleccionada.");
+                System.out.println(Elpayasodelmal.getNombre());
+                System.out.println(Elpayasodelmal.getAnio());
+                System.out.println(Elpayasodelmal.getDirector());
+                System.out.println(Elpayasodelmal.getPais());
+                break;
+            case 12:
+                System.out.println("Opción 12 seleccionada.");
+                System.out.println(It.getNombre());
+                System.out.println(It.getAnio());
+                System.out.println(It.getDirector());
+                System.out.println(It.getPais());
+                break;
+            default:
+                System.out.println("Número inválido. Debe ingresar un número del 1 al 12.");
+                break;
+        }
 
-        System.out.println("Datos de tu género favorito:");
-        System.out.println(favorito.tipo);
-        System.out.println(favorito.getAnio());
-        System.out.println(favorito.getPais());
-        System.out.println(favorito.getArtista());
     }
+
 }
+
 
--- Clase 05/Peliculas/src/Peliculas.java
+++ Clase 05/Peliculas/src/Peliculas.java
@@ -2,20 +2,28 @@
 public class Peliculas {
 
     //Atributos
-    String tipo;
+    String nombre;
+    String genero;
     int anio;
-    String artista;
+    String director;
     String pais;
 
-    public Peliculas(String tipo, String ritmo, int anio, String artista, String pais) {
-        //Metodos
-        this.tipo = tipo;
-        this.anio = anio;
-        this.artista = artista;
-        this.pais = pais;
+    //Setter y Getters
+    public String getNombre() {
+        return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 
     public int getAnio() {
         return anio;
@@ -25,12 +33,12 @@ public class Peliculas {
         this.anio = anio;
     }
 
-    public String getArtista() {
-        return artista;
+    public String getDirector() {
+        return director;
     }
 
-    public void setArtista(String artista) {
-        this.artista = artista;
+    public void setDirector(String director) {
+        this.director = director;
     }
 
     public String getPais() {
@@ -40,8 +48,15 @@ public class Peliculas {
     public void setPais(String pais) {
         this.pais = pais;
     }
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
+
+    //Metodos
+    public Peliculas(String nombre, String genero, int anio, String director, String pais) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.anio = anio;
+        this.director = director;
+        this.pais = pais;
     }
+
 
 }
